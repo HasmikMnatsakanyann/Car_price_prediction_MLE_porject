17b117e165fe62710a220d2e53c2fd178c72213d
Aram Bughdaryan <arambughdaryan@gmail.com>
2023-12-17 23:11:57 +0400

    added streamlit

--- CarPricePredictor/model.py
+++ CarPricePredictor/model.py
@@ -1,6 +1,6 @@
 import xgboost as xgb
 import pkg_resources
-from .preprocess import preprocess_single_input
+from preprocess import preprocess_single_input
 import logging
 
 class CarPricePredictor:
@@ -12,25 +12,17 @@ class CarPricePredictor:
 
     def predict_single_input(self, input):
         """
-        Function to predict price on a single input of the form : {
-                "Car": ["Toyota Camry", "Honda Accord"],
-                "Vehicle Type": ["Sedan", "Sedan"],
-                "Wheel left/right": ["Left", "Right"],
-                "Color": ["Red", "Blue"],
-                "Transmission": ["Automatic", "Manual"],
-                "Mileage": [50000, 30000],
-                "Year": [2018, 2019]
-            }
+        Function to predict price on a single input:
         Input: Dict as shown in example
         usage example :
         sample = {
-"Car": "Toyota Camry",
-"Vehicle Type": "Sedan",
-"Wheel left/right": "Left",
-"Color": "Red",
-"Transmission": "Automatic",
-"Mileage": 50000,
-"Year": 2018
+            "Car": "Toyota Camry",
+            "Vehicle Type": "Sedan",
+            "Wheel left/right": "Left",
+            "Color": "Red",
+            "Transmission": "Automatic",
+            "Mileage": 50000,
+            "Year": 2018
             }
         price_predictor = CarPricePredictor()
         predicted_price = price_predictor.predict_single_input(sample)
@@ -38,5 +30,5 @@ class CarPricePredictor:
         preprocessed_input = preprocess_single_input(input)
         dmat_data = xgb.DMatrix(preprocessed_input)
         predictions = self.model.predict(dmat_data)
-        return predictions
+        return round(predictions[0])
 
